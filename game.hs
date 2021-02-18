1efc971349cad79128664cde958ccf8bccdf6790
William Berger <william.berger.7099@student.uu.se>
2021-02-18 03:15:48 +0100

    + placeShip, validShipPlacement, noCollision etc

--- game.hs
+++ game.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE LambdaCase #-} 
+
+------------------- Imports -------------------
 import Data.Array
-                                     
-                                                         
+------------------- Imports -------------------
+
 data Cell = Empty SquareState | Ship SquareState deriving (Show, Eq)     
 data SquareState = Checked | NotChecked deriving (Show, Eq)        
 data Direction = Horizontal | Vertical deriving (Show, Eq)   
@@ -11,24 +13,26 @@ type Row = Int
 type Col = Int                                           
 type Coordinates = (Row, Col)
 
-type Size = Int
-                                                         
-initBoard :: Size -> Array (Int, Int) Cell                
+type BoardSize = Int
+type ShipSize = Int
+
+-- Create a new board, a 2d array, where all cells are empty notchecked initially.                                                         
+initBoard :: BoardSize -> Board        
 initBoard s = array boardIndex $ zip (range boardIndex) (repeat $ Empty NotChecked)
              where boardIndex = ((0, 0), (s - 1, s - 1))  
 
--- PRE: coordinates are in range
+-- PRE: coordinates are in range, cell is NotChecked
 -- Changes the state of a cell to checked
-updateCell :: Board -> Coordinates -> Board
-updateCell b (r, c) = case getCell b (r, c) of
+checkCell :: Board -> Coordinates -> Board
+checkCell b (r, c) = case getCell b (r, c) of
                       Empty NotChecked -> b // [((r,c), Empty Checked)]   
                       Ship NotChecked  -> b // [((r,c), Ship Checked)]
 
 -- Give size of board, and coordinates
 -- returns true if coordinates are in boardIndex, else false
-validCoordinates :: Size -> Coordinates -> Bool
-validCoordinates s (r, c) = inRange boardIndex (r, c)
-                           where boardIndex = ((0, 0), (s - 1, s - 1))  
+validCoordinates :: BoardSize -> Coordinates -> Bool
+validCoordinates s = inRange boardIndex
+                    where boardIndex = ((0, 0), (s - 1, s - 1))  
 
 -- PRE: coordinates are in range
 -- returns the cell at coordinates
@@ -42,42 +46,35 @@ getState b c =  case getCell b c of
                      Empty s -> s
                      Ship s -> s
 
--- PRE: cell is empty at coordinates
--- place down a shipcell at given coordinates
-placeShipCell :: Board -> Coordinates -> Board
-placeShipCell b (r, c) = b // [((r,c), Ship NotChecked)]   
-
 -- PRE: coordinates are in range, ship placement wont go out of bounds
---      if direction == horizontal, the coordinates is the top part of the ship,
---      if vertical its the far most left part of the ship
--- examples: if size 3, coord c, direction vertical:      s (coord)
---                                                        s
---                                                        s
+placeShip :: Board -> Coordinates -> ShipSize -> Direction -> Board
+placeShip b _ 0 _= b
+placeShip b (r, c) s Horizontal = placeShip (b // [((r, c), Ship NotChecked)]) (r, c + 1) (s - 1) Horizontal
+placeShip b (r, c) s Vertical = placeShip (b // [((r, c), Ship NotChecked)]) (r + 1, c) (s - 1) Vertical
 
-placeShip :: Board -> Coordinates -> Size -> Direction -> Board
-placeShip b coords s d = case d of
-                        Horizontal -> placeHorizontal b coords s
-                        Vertical   -> placeVertical b coords s
+validShipPlacement :: Board -> Coordinates -> ShipSize -> Direction -> Bool
+validShipPlacement b (r, c) s Horizontal = validCoordinates (getBoardSize b) (r + s - 1, c) 
+                                           && noCollision b (r, c) s Horizontal  -- since one of the ship parts are placed in
+validShipPlacement b (r, c) s Vertical   = validCoordinates (getBoardSize b) (r, c + s - 1) 
+                                           && noCollision b (r, c) s Vertical   -- the cell of the coordinates we subtract 1
 
-{- 
-  We need to implement following, somewhere along the way:
-* Handle the case when coordinates are out of range (incl. when any part's coordinates are out of range)
-* Handle the case when ny ship part crosses another ship part already placed, 
-  thus rendering the placement invalid.
--}
+-- cheeck if the ship the user want to place will collide with any existing ships
+noCollision :: Board -> Coordinates -> ShipSize -> Direction -> Bool
+noCollision b _ 0 _ = True
+noCollision b (r, c) s Horizontal | b ! (r, c) == Ship NotChecked = False
+                                  | otherwise = noCollision b (r, c + 1) (s - 1) Horizontal
+noCollision b (r, c) s Vertical   | b ! (r, c) == Ship NotChecked = False
+                                  | otherwise = noCollision b (r + 1, c) (s - 1) Vertical                               
 
-placeHorizontal :: Board -> Coordinates -> Size -> Board
-placeHorizontal b _ 0 = b
-placeHorizontal b (r, c) s = placeHorizontal (b // [((r, c), Ship NotChecked)]) (r, c + 1) (s - 1)
-
-placeVertical :: Board -> Coordinates -> Size -> Board
-placeVertical b _ 0 = b
-placeVertical b (r, c) s = placeVertical (b // [((r, c), Ship NotChecked)]) (r + 1, c) (s - 1)
+-- returns boardsize from given board
+getBoardSize :: Board -> BoardSize
+getBoardSize b =  round . sqrt . fromIntegral $ length b
 
 
+-- Should only be called after all ships have been placed
 -- Check if all ships are Checked
 winner :: Board -> Bool
-winner b = not(any (\cell -> cell == Ship NotChecked) b)
+winner b = not $ any (\cell -> cell == Ship NotChecked) b
 
 -- Board to list of cells
 cellList :: Board -> [Cell]
@@ -89,11 +86,13 @@ stateList :: Board -> [SquareState]
 stateList b = map (\case (Empty s) -> s; (Ship s) -> s) (elems b)
 
 
+arrayToFile :: Board -> IO()
+arrayToFile b = undefined
+
+b :: Board
 b = initBoard 10
 
-
 {- 
-
 When player1 is playing their action manipulates player2's array. What's being displayed should just be the states (and in case of ship, display in other way)
 
 Drawing when it's player1's turn:
@@ -103,6 +102,4 @@ If is is checked and a ship, display it in another way
 Shooting:
 
 If input is valid, update player2's array and call the draw func
-
-
 -}
