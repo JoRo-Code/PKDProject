8ffbd85e7f7fdf4fceb02f2df70db0565a8f1292
Jonas Rosengren <jonas@rosengren.info>
2021-02-20 10:43:01 +0100

    fixed bug for starting pos of horizontal lines

--- src/rendering.hs
+++ src/rendering.hs
@@ -9,18 +9,21 @@ boardGridColor = makeColorI 255 255 255 255
 type BoardPos = ((Float,Float),(Float,Float))
 
 screenWidth :: Int
-screenWidth = 640
+screenWidth = 1400
 screenHeight :: Int
-screenHeight = 460
+screenHeight = 860
 
 cellWidth width n =  width / fromIntegral n
 
 cellHeight height n =  height / fromIntegral n
 
 shootingBoardPos :: BoardPos
-shootingBoardPos = ( ((fromIntegral screenWidth)*0.5,                             0),
-                     (fromIntegral screenWidth,         fromIntegral screenHeight) )
+shootingBoardPos = (((fromIntegral screenWidth)*0.5,0),(fromIntegral screenWidth, fromIntegral screenHeight))
 
+{-
+    ( ((fromIntegral screenWidth)*0.5,                             0),
+                     (fromIntegral screenWidth,         fromIntegral screenHeight) )
+-}
 
 boardWidth ((x1,y1),(x2,y2)) = x2-x1
 boardHeight ((x1,y1),(x2,y2)) = y2-y1
@@ -42,10 +45,10 @@ shootingBoardCellHeight = cellHeight shootingBoardHeight 10
 boardGrid cellWidth cellHeight boardPos@((x1,y1),(x2,y2)) =
     pictures
     $ concatMap (\i -> [ line [ (x1 + i * cellWidth, 0.0)
-                              , (x1 + i * cellWidth,  boardWidth boardPos)
+                              , (x1 + i * cellWidth,  boardHeight boardPos)
                               ]
-                       , line [ (y1 + 0.0,                      i * cellHeight)
-                              , (y1 + boardWidth boardPos, i * cellHeight)
+                       , line [ (x1 + 0.0,                      i * cellHeight)
+                              , (x1 + boardWidth boardPos, i * cellHeight)
                               ]
                        ])
       [0.0 .. fromIntegral n]
