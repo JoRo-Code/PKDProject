a33efed2c479c751ec6612ebfb4e85f4d6189727
William Brunnsberg <william.brunnsberg.7068@student.uu.se>
2021-02-21 19:45:06 +0100

    Added fuctions to ai

--- ai.hs
+++ ai.hs
@@ -5,13 +5,10 @@ module AI where
 ------------------- Imports -------------------
 import Data.Array
 import System.IO
-import System.Random
-import Graphics.Gloss
 ------------------- Imports -------------------
 
 data Cell = Empty SquareState | Ship SquareState deriving (Show, Eq)     
-data SquareState = Checked | NotChecked deriving (Show, Eq)        
-data Direction = Horizontal | Vertical deriving (Show, Eq)   
+data SquareState = Checked | NotChecked deriving (Show, Eq)         
 
 type Board = Array (Row, Col) Cell                  
 type Row = Int                                           
@@ -19,7 +16,9 @@ type Col = Int
 type Coordinates = (Row, Col)
 
 type BoardSize = Int
-type ShipSize = Int
+
+type ShootList = [(Coordinates,Cell)]
+type Stack = [(Coordinates,Cell)]
 
 -- Create a new board, a 2d array, where all cells are empty notchecked initially.                                                         
 initBoard :: BoardSize -> Board        
@@ -51,80 +50,86 @@ getState b c =  case getCell b c of
                      Empty s -> s
                      Ship s -> s
 
--- PRE: coordinates are in range, ship placement wont go out of bounds
-placeShip :: Board -> Coordinates -> ShipSize -> Direction -> Board
-placeShip b _ 0 _= b
-placeShip b (r, c) s d = placeShip (b // [((r, c), Ship NotChecked)]) (r + rowOffset, c + colOffset) (s - 1) d
-                        where (rowOffset, colOffset) = offset d
-
-validShipPlacement :: Board -> Coordinates -> ShipSize -> Direction -> Bool
-validShipPlacement b (r, c) s d = validCoordinates (getBoardSize b) (r + rowOffset * s - 1, c + colOffset * s - 1) -- (-1) because the ship part on (r, c) is included
-                                && validCoordinates (getBoardSize b) (r, c) 
-                                && noCollision b (r, c) s d
-                                where (rowOffset, colOffset) = offset d
-
--- cheeck if the ship the user want to place will collide with any existing ships
-noCollision :: Board -> Coordinates -> ShipSize -> Direction -> Bool
-noCollision b _ 0 _ = True
-noCollision b (r, c) s d | b ! (r, c) == Ship NotChecked = False
-                         | otherwise = noCollision b (r + rowOffset, c + colOffset) (s - 1) d
-                           where (rowOffset, colOffset) = offset d
--- calculate offset in row and col
-offset :: Direction -> (Row, Col)
-offset Vertical = (1, 0)
-offset Horizontal = (0, 1)
-
 -- returns boardsize from given board
 getBoardSize :: Board -> BoardSize
 getBoardSize b =  round . sqrt . fromIntegral $ length b
 
--- Should only be called after all ships have been placed
--- Check if all ships are Checked
-winner :: Board -> Bool
-winner b = not $ any (\cell -> cell == Ship NotChecked) b
 
--- Board to list of cells
-cellList :: Board -> [Cell]
-cellList = elems 
+--------------------- AI --------------------------------
 
--- Board to list of state
--- Used to print other players board, but without displaying ships, just where player have already checked.
-stateList :: Board -> [SquareState]
-stateList b = map (\case (Empty s) -> s; (Ship s) -> s) (elems b)
+-- Creates a ShootList from a row in a board
+aiShootListRow :: Board -> Row -> ShootList
+aiShootListRow board r = aiShootListRowAux board (r,0)
+  where aiShootListRowAux :: Board -> Coordinates -> ShootList
+        aiShootListRowAux board (r,c) | c == getBoardSize board = []
+                                      | otherwise = ((r,c),getCell board (r,c)) : aiShootListRowAux board (r,c+1)
 
-b :: Board
-b = initBoard 10
+-- Creates a ShootList from all rows in a board
+aiShootList :: Board -> ShootList
+aiShootList board = aiShootListAux board 0
+  where aiShootListAux :: Board -> Row -> ShootList
+        aiShootListAux board r | r == getBoardSize board = []
+                               | otherwise = aiShootListRow board r ++ aiShootListAux board (r+1)
 
-------------------- File -------------------
+-- Sorts a shootlist so AI prioritises cells that are not next to each other
+aiPrio :: ShootList -> ShootList
+aiPrio l = aiPrioAux l []
+  where aiPrioAux :: ShootList -> ShootList -> ShootList
+        aiPrioAux [] acc = acc
+        aiPrioAux (x:xs) acc 
+          | even $ getRow x = if  odd $ getCol x then aiPrioAux xs ([x] ++ acc) else aiPrioAux xs (acc ++ [x])
+          | otherwise       = if even $ getCol x then aiPrioAux xs ([x] ++ acc) else aiPrioAux xs (acc ++ [x])
 
-path :: FilePath 
-path = "fileTest.txt"
+-- Returns column of element in a ShootList
+getCol :: (Coordinates,Cell) -> Col
+getCol ((_,b),_) = b
 
-arrayToFile :: Board -> IO()
-arrayToFile b = do
-     appendFile path $ show (elems b)
+-- Returns row of element in a ShootList
+getRow :: (Coordinates,Cell) -> Row
+getRow ((a,_),_) = a
 
-fileToArray :: IO ()
-fileToArray = do
-    handle  <- openFile path ReadMode
-    content <- hGetContents handle
-    putStr content
-    hClose handle
+-- Returns True if a Cell contains an unchecked Ship, otherwise False
+aiHunt :: (Coordinates,Cell) -> Bool
+aiHunt ((_,_),Ship NotChecked) = True
+aiHunt ((_,_),_) = False
 
-------------------- File -------------------
+-- Checks first cell in stack
+aiShoot :: (Board,Stack) -> (Board,Stack)
+aiShoot (b,s) = aiShootAux (b,s) (filterShootList (aiPrio (aiShootList b)) s)
 
-{- 
-When player1 is playing their action manipulates player2's array. What's being displayed should just be the states (and in case of ship, display in other way)
-
-Drawing when it's player1's turn:
-Go through the array and draw a cross at every element that is checked. 
-If is is checked and a ship, display it in another way
-
-Shooting:
-
-If input is valid, update player2's array and call the draw func
--}
+-- Checks first cell in stack
+--PRE: ShootList is not empty
+aiShootAux :: (Board,Stack) -> ShootList -> (Board,Stack)
+aiShootAux (b,[]) l = aiShootAux (b,[head l]) (tail l) -- Adds the head of ShootList to Stack if Stack is empty
+aiShootAux (b,s) l 
+  | aiHunt (head s) = (checkCell b (fst $ head s),cohesiveCells b (head s) ++ tail s)
+  | otherwise = (checkCell b (fst $ head s),tail s)
 
 
---aiPlaceShip :: Board -> Coordinates -> ShipSize -> Direction -> Board
---aiPlaceShip 
+-- Removes all checked cells and cells that are in Stack from ShootList
+filterShootList :: ShootList -> Stack -> ShootList
+filterShootList [] _ = []
+filterShootList (x:xs) stack = filterShootListAux x stack ++ filterShootList xs stack
+  where filterShootListAux ((_,_),Empty Checked) _ = []
+        filterShootListAux ((_,_),Ship Checked) _ = []
+        filterShootListAux ((r,c),cell) stack | notElem ((r,c),cell) stack = [((r,c),cell)]
+                                              | otherwise = []
+
+-- Returns a Stack of the cohesive cells to a cell in a row
+cohesiveCellsRow :: Board -> (Coordinates,Cell) -> Stack
+cohesiveCellsRow b ((r,c),x) 
+  | validCoordinates (getBoardSize b) (r,c-1) && validCoordinates (getBoardSize b) (r,c+1) = [((r,c-1),getCell b (r,c-1)), ((r,c+1),getCell b (r,c+1))]
+  | validCoordinates (getBoardSize b) (r,c-1) = [((r,c-1),getCell b (r,c-1))]
+  | validCoordinates (getBoardSize b) (r,c+1) = [((r,c+1),getCell b (r,c+1))]
+  | otherwise = []
+
+-- Returns a Stack of all cohesive cells to a cell
+cohesiveCells :: Board -> (Coordinates,Cell) -> Stack
+cohesiveCells b ((r,c),x) 
+  | validCoordinates (getBoardSize b) (r-1,c) && validCoordinates (getBoardSize b) (r+1,c) = [((r-1,c),getCell b (r-1,c)), ((r+1,c),getCell b (r+1,c))] ++ cohesiveCellsRow b ((r,c),x)
+  | validCoordinates (getBoardSize b) (r-1,c) = [((r-1,c),getCell b (r-1,c))] ++ cohesiveCellsRow b ((r,c),x)
+  | validCoordinates (getBoardSize b) (r+1,c) = [((r+1,c),getCell b (r+1,c))] ++ cohesiveCellsRow b ((r,c),x)
+  | otherwise = cohesiveCellsRow b ((r,c),x)
+
+tester :: (Array (Int, Int) Cell, [a])
+tester = (array ((0,0),(2,2)) [((0,0),Empty NotChecked),((0,1),Empty NotChecked),((0,2),Ship NotChecked),((1,0),Empty NotChecked),((1,1),Empty NotChecked),((1,2),Ship NotChecked),((2,0),Ship NotChecked),((2,1),Ship NotChecked),((2,2),Empty NotChecked)],[])
