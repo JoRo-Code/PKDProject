06112247b72cbd33dbd38a0c9d0aa63ff6381fe2
Jonas Rosengren <jonas@rosengren.info>
2021-02-24 19:41:16 +0100

    added main specifications, keeping track of gen

--- src/main.hs
+++ src/main.hs
@@ -2,42 +2,67 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Data.Color
+import System.Random
 
 import Game         -- initialGame
 import Logic        -- eventHandler
 import Rendering    -- drawGame
-import System.Random
 
 
 
+{- allCoords
+       RETURNS: all possible cellcoords on board regarding global n
+
+-}
 allCoords :: [CellCoord]
 allCoords = [(c, r) | c <- [0..n-1], r <- [0..n-1]]
 
 
+{- findValidDirectionalPlacements board coords ship direction
+       RETURNS: possible coords of ship with direction
+
+-}
 findValidDirectionalPlacements :: Board -> [CellCoord] -> ShipSize ->  Direction -> [(CellCoord, Direction)]
 findValidDirectionalPlacements b coords s d = map (\coord -> (coord, d)) $ filter (\coord -> validShipPlacement b coord s d) coords
                  
 
+
+{- findAllValidPlacements board ship 
+       RETURNS: all possible placements of ship on board
+-}
 findAllValidPlacements :: Board -> ShipSize -> [(CellCoord, Direction)]
 findAllValidPlacements b s = findValidDirectionalPlacements b allCoords s Horizontal ++ findValidDirectionalPlacements b allCoords s Vertical 
 
 
-randomPlacement :: StdGen ->  [(CellCoord, Direction)]  -> ((CellCoord, Direction), StdGen)
-randomPlacement gen list = (list !! randomInt, newGen)
+{- randomElement list gen
+    RETURNS: (randomly generated element of list, finalGen)
+-}
+randomElement :: [a] -> StdGen -> (a, StdGen)
+randomElement list gen = (list !! randomInt, newGen)
                      where range = (0, length list - 1)
                            (randomInt, newGen) = randomR range gen
 
+{- placeShipAI gen board ship placements
+       updates board with a random placement of ship
+
+-}
 placeShipAI :: StdGen -> Board -> ShipSize -> [(CellCoord, Direction)] -> (Board, StdGen)
 placeShipAI gen b s placements = (placeShipAux b coord s d, newGen)
-                             where ((coord , d), newGen) = randomPlacement gen placements --randomPlacement placements -- call random function here
+                             where ((coord , d), newGen) = randomElement gen placements
 
 
-placeMultipleShipsAI :: StdGen -> Board -> Ships -> Board
-placeMultipleShipsAI gen b [] = b
+{- placeMultipleShipsAI gen board ships
+       places the ships on random places on the board
+       RETURNS: (generated board, newGen)
+-}
+
+placeMultipleShipsAI :: StdGen -> Board -> Ships -> (Board, StdGen)
+-- VARIANT: length ships
+placeMultipleShipsAI gen b [] = (b, gen)
 placeMultipleShipsAI gen b ((_, _, s):ships) = placeMultipleShipsAI newGen newBoard ships
                                       where (newBoard, newGen) = placeShipAI gen b s (findAllValidPlacements b s)
 
-
+-- window specifications
 window :: Display
 window = InWindow "BattleShips" (floor screenWidth, floor screenHeight) (100, 100)
 
@@ -47,10 +72,15 @@ backgroundColor = makeColor 0 0 0 0
 fps :: Int
 fps = 30
 
+
+
+{- main
+       main loop which controls output in gloss
+-}
 main :: IO ()
 main = do
        gen <- getStdGen
-       let initGameBoardAI =  placeMultipleShipsAI gen (gameBoardAI initGame) initShips
-       play window backgroundColor fps initGame {gameBoardAI = initGameBoardAI, gen = gen} drawGame eventHandler  (const id)
+       let (initGameBoardAI, newGen) =  placeMultipleShipsAI gen (gameBoardAI initGame) initShips
+       play window backgroundColor fps initGame {gameBoardAI = initGameBoardAI, gen = newGen} drawGame eventHandler  (const id)
 
 
