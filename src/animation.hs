372fe4dbcacb7c8d598f0534286dcd7903aa87a3
William Brunnsberg <william.brunnsberg.7068@student.uu.se>
2021-03-01 20:41:24 +0100

    Bug

--- src/animation.hs
+++ src/animation.hs
@@ -4,26 +4,38 @@ import Game
 
 
 {- animationFunc time game
-    updates radius of shootanimation according to time
-    also makes sure to change from show to hidden when outside of boundary
+    updates shooting and radar animation with respect to time.
+    RETURNS: game with new angle of radarAnimation, and new radius and derivative for shootingAnimation and whether or not shootingAnimation should be shown with respect to time
+    EXAMPLES:
+                    increasing radius               -> shootAnimation explodes
+                    decreasing derivative of radius -> shootAnimation fades out
+                    showBool                        -> hides explosion at end radius
+
+                    increasing angle of radius      -> spinning radar
+                    
 -}
 animationFunc :: Float -> Game -> Game
-animationFunc dt game  = game {shootAnimation = (hitShip, newRadiusExplosion r d, pos, end, newD d, showExplosion r b),
+animationFunc dt game  = game {shootAnimation = (hitShip, newRadius r d, pos, end, newD d, showExplosion r b),
                                radarAnimation = (radiuses, newAngle angle)
                                 } 
             where
                 (hitShip, r, pos, end, d, b) = shootAnimation game 
                 (radiuses, angle) = radarAnimation game
+
+                --  derivative of shootAnimation decreases with time -> fades explosion
                 newD d = d*0.95
-                newRadiusExplosion r d
+
+                --  radius of shootAnimation increases with respect to derivative -> explodes
+                newRadius r d
                             | abs r >= end = 0
                             | otherwise = r + d*dt
+
+                --  shootAnimation gets hidden if it has reached its end boundary -> stops showing
                 showExplosion r b 
                                 | abs r >= end = False
                                 | otherwise = b
-                newRadius r 
-                            | abs r >= screenWidth = 0
-                            | otherwise = r + 0.1 + 0.5 * r*dt + r*dt
+
+                --  angle of radarAnimation increases with time -> spins
                 newAngle angle | angle == 360.0 = 0.0
                                | otherwise = angle + 4.0
 
--- src/shuffle.hs
+++ src/shuffle.hs
@@ -2,7 +2,13 @@ module Shuffle where
 import System.Random
 
 {- shuffle list seed
-   shuffles a list with a seed
+    shuffles a list with a seed
+    RETURNS: (shuffled list with seed, newGen of seed)
+    EXAMPLES: 
+                shuffle [1..5] (mkStdGen 10)                    == ([2,3,1,5,4],                   StdGen {unStdGen = SMGen 10920071015943500239 614480483733483467})
+                shuffle [] (mkStdGen 10)                        == ([],                            mkStdGen 10)
+                shuffle "hello" (mkStdGen 10)                   == ("elhol",                       StdGen {unStdGen = SMGen 10920071015943500239 614480483733483467})
+
 -}
 shuffle :: [a] -> StdGen -> ([a], StdGen)
 shuffle [] gen = ([], gen)
@@ -10,9 +16,15 @@ shuffle list gen = shuffleAux list [] gen
 
 
 {- shuffleAux list newList seed
-    newly randomly generated newList of list with seed
--}
+    adds shuffled list to newList
+    RETURNS: (shuffled list with seed concatenated with newList, newGen of seed) 
+    EXAMPLES:   
+                shuffleAux [1..5]  []        (mkStdGen 10)      == ([2,3,1,5,4],                    StdGen {unStdGen = SMGen 10920071015943500239 614480483733483467})
+                shuffleAux [1..5]  [6..9]    (mkStdGen 10)      == ([2,3,1,5,4,6,7,8,9],            StdGen {unStdGen = SMGen 10920071015943500239 614480483733483467})
+                shuffleAux []      [6..9]    (mkStdGen 10)      == ([6..9],                         mkStdGen 10)
+                shuffleAux "hello" " world"  (mkStdGen 10)      == ("elhol world",                  StdGen {unStdGen = SMGen 10920071015943500239 614480483733483467})
 
+-}
 shuffleAux :: [a] -> [a] -> StdGen -> ([a], StdGen)
 -- VARIANT: length list
 shuffleAux [] shuffledList gen = (shuffledList, gen)
@@ -24,9 +36,16 @@ shuffleAux list randomList gen = shuffleAux updatedList (pickedElement:randomLis
             updatedList = removeIndex list randomIndex
 
 
-
 {- removeIndex list index 
+    removes element of list at index
+    PRE: -
     RETURNS: a list with the element at position index removed
+    EXAMPLES:   
+                removeIndex [1,2,3] 1       == [1,3]
+                removeIndex [1,2,3] (-1)    == [1,2,3]
+                removeIndex []      1       == []
+                removeIndex "hello" 3       == "helo"
+
 -}
 removeIndex :: [b] -> Int -> [b]
 removeIndex list index = map snd $ filter (\(i, element) -> i /= index) (zip [0..length list - 1] list)
